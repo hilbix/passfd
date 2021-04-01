4033cc51b8dd02e44ab7e3765b279209bfbc57fa
Valentin Hilbig <webmaster@scylla-charybdis.com>
2021-04-01 02:56:03 +0200

    Test.sh added

---
+++ Test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STDOUT() { local e=$?; printf '%q' "$1"; printf ' %q' "${@:2}"; printf '\n'; return $e; }
+STDERR() { local e=$?; STDOUT "$@" >&2; return $e; }
+OOPS() { STDERR Test fail: "$@"; exit 23; }
+o() { "$@" || OOPS rc=$?: "$@"; STDOUT ok: "$@"; }
+
+S=.tmp/test.sock
+
+o ./passfd v l i "$S" 0 <<< 'hello world' -- ./passfd v o "$S" 7 -- bash -c 'exec cmp <(echo hello world) - <&7'
+[ -e "$S" ] && OOPS socket still exists: "$S"
+
+:
+
